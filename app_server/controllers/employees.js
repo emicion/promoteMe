24f3564be6545acb6a5180f897c24d660a535710
Christopher Calderon <emicion@gmail.com>
2019-05-16 23:32:57 -0700

    Lots of changes to CSS and functionality

--- app_server/controllers/employees.js
+++ app_server/controllers/employees.js
@@ -1,5 +1,27 @@
 const employeeList = (req, res) => {
-  res.render('index', { title: 'Employee List'} );
+  res.render('employee-list', {
+    title: 'Employee Feed',
+    pageHeader: {
+      title: 'Today is the Day ',
+      strapline: 'Help your employees achieve their goals!'
+    },
+    employees: [{
+      name: 'Masahiro Takahashi',
+      position: 'Meat Cutter',
+      years: 3,
+      accomplishments: ['Fastest Wrapper', 'Best Customer Service', 'Cleanest Work Station', 'Never Late', 'Expert Gourmet']
+    },{
+      name: 'Risa Mizoguchi',
+      position: 'Meat Clerk',
+      years: 1,
+      accomplishments: ['Fastest Wrapper', 'Best Customer Service', 'Cleanest Work Station', 'Never Late', 'Expert Gourmet']
+    }, {
+      name: 'Yama Iriguchi',
+      position: 'Senior Meat Clerk',
+      years: 5,
+      accomplishments: ['Fastest Wrapper', 'Best Customer Service', 'Cleanest Work Station', 'Never Late', 'Expert Gourmet']
+    }]
+  });
 };
 
 const employeeInfo = (req, res) => {
